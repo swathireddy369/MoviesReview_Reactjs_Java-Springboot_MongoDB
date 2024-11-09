f4a8ee403ee20885df01159f5f7cba3e2a17ecb1
swathireddy369 <swathiamaravadi53@gmail.com>
2024-11-09 15:44:58 -0500

    initial commit

---
+++ src/test/java/dev/swathi/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.swathi.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
