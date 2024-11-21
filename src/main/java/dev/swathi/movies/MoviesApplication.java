8198fa98ecb95210f1efabfc76b8423afab84c82
swathireddy369 <swathiamaravadi53@gmail.com>
2024-11-21 12:22:53 -0500

    done

--- src/main/java/dev/swathi/movies/MoviesApplication.java
+++ src/main/java/dev/swathi/movies/MoviesApplication.java
@@ -8,9 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class MoviesApplication {
-
-
-	public static void main(String[] args) {
+     public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 
 	}
